527f2b8aea3d2da514c35078641e4de878620e39
Niklas Beitler <niklas.beitler@duh-group.com>
2026-05-11 10:33:33 +0200

    activates update checker

--- src/controller/Update.py
+++ src/controller/Update.py
@@ -3,12 +3,12 @@ import os
 from tkinter import messagebox
 from view.ProgressWindow import ProgressWindow
 
-
-def install_update(model, repo_url):
+def check_for_update(model, repo_url):
     try:
         api_url = f"{repo_url}/releases/latest"
 
         response = requests.get(api_url)
+        print(response)
         if response.status_code == 200:
             latest_release = response.json()
             latest_version = latest_release["tag_name"]
@@ -18,8 +18,9 @@ def install_update(model, repo_url):
                 title = latest_release["name"]
                 description = latest_release["body"]
                 if user_accepts_update(model.app_version, latest_version, description, title):
-                    download_and_install_update(model, download_url, latest_version)
-                    return True
+                    if downloadet_update(model, download_url, latest_version):
+                        messagebox.showinfo("Update Heruntergeladen", f"Das Update wurde erfolgreich heruntergeladen und liegt nun im downloads verzeichnis. Bitte installiere nun das Update.")
+                        return True
         else:
             print("Fehler beim Abrufen der Update-Informationen.")
         return False
@@ -30,10 +31,10 @@ def install_update(model, repo_url):
 
 def user_accepts_update(current_version, latest_version, description, title):
     # print(f"Ein neues Update ist verfügbar!")
-    return messagebox.askyesno("Update verfügbar", f"Eine neue Version ist verfügbar!\n\nAktuelle Version:{current_version}\nNeue Version: {latest_version}\n\nWas macht das neue Update:\n{description}\n\nJetzt installieren?")
+    return messagebox.askyesno("Update verfügbar", f"Eine neue Version ist verfügbar!\n\nAktuelle Version:{current_version}\nNeue Version: {latest_version}\n\nWas macht das neue Update:\n{description}\n\nJetzt herunterladen?")
 
 
-def download_and_install_update(model, download_url, latest_version):
+def downloadet_update(model, download_url, latest_version):
     progress_window = ProgressWindow(model)
     progress_window.set_progress(30, "Wird heruntergeladen...", 0.05)
 
@@ -50,7 +51,6 @@ def download_and_install_update(model, download_url, latest_version):
 
         progress_window.set_progress(80, "Wird installiert...", 0.05)
 
-        execute_installer(download_path)
 
         progress_window.set_progress(100, "Wird installiert...", 0.02)
 
@@ -58,8 +58,3 @@ def download_and_install_update(model, download_url, latest_version):
         print(f"Error downloading the update: {e}")
 
     progress_window.mainloop()
-
-
-def execute_installer(installer_path):
-    import subprocess
-    subprocess.Popen([installer_path])
