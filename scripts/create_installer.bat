54f22c742f0abbac4c9b779e69077db0fcf3dcc3
Niklas Beitler <niklas.beitler@duh-group.com>
2026-07-21 11:59:19 +0200

    changes sign hash

--- scripts/create_installer.bat
+++ scripts/create_installer.bat
@@ -13,7 +13,7 @@ set "SCRIPT=%~dp0DUH_Startcenter.iss"
 
 "%ISCC%" "%SCRIPT%"
 
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe" sign /sha1 "25b8123c7ea491f66dc913e37d668a7297f35ab6" /t http://time.certum.pl /fd SHA256 /v %BASE%output\installer\DUH_Startcenter-installer.exe
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe" sign /sha1 "d578eded17dbc01750709680db13b5b1273edb12" /t http://time.certum.pl /fd SHA256 /v %BASE%output\installer\DUH_Startcenter-installer.exe
 
 pause
 endlocal
--- scripts/sign_exe.bat
+++ scripts/sign_exe.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe" sign /sha1 "25b8123c7ea491f66dc913e37d668a7297f35ab6" /t http://time.certum.pl /fd SHA256 /v DUH_Startcenter.exe
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe" sign /sha1 "d578eded17dbc01750709680db13b5b1273edb12" /t http://time.certum.pl /fd SHA256 /v DUH_Startcenter.exe
 
 pause
 endlocal
